9be22749513bec25d471c7a8d9cc10ada25d20c9
devmahir <mdmahir.ds@gmail.com>
2023-03-27 15:01:52 +0600

    first commit

---
+++ client/src/components/Header/MainHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MainHeader = () => {
+  return (
+    <div>MainHeader</div>
+  )
+}
+
+export default MainHeader;
+
+
+git init
+git add.
+git commit - m "first commit"
+git branch - M main
+git remote add origin https://github.com/dev-mahir/react-yarn-sass-template.git
+git push - u origin main
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <h2>We love js very much .</h2>
+        <button className='btn'>Download</button>
+      </div>
+
+
+    </>
+  )
+}
+
+export default Home
